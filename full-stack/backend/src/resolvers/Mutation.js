3eefe6d42baea009aab5d79f03f130cbb84c3b87
Roodey <joey8williams@gmail.com>
2018-11-02 22:34:07 -0400

    Plugged the DB into our backend, insert inuendos as necessary

--- full-stack/backend/src/resolvers/Mutation.js
+++ full-stack/backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const Mutations = {};
+const Mutations = {
+  async createStory(parent,args,ctx,info){
+    //TODO: Check if authenticated
+
+    const item = await ctx.db.mutation.createStory({
+      data:{
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+//  createDog(parent, args, context, info){
+//    global.dogs = global.dogs || [];
+//    //create a dog
+//    const newDog = {name:args.name};
+//    global.dogs.push(newDog);
+//    return newDog;
+//  }
+};
 
 module.exports = Mutations;
--- full-stack/backend/src/resolvers/Query.js
+++ full-stack/backend/src/resolvers/Query.js
@@ -1,8 +1,18 @@
-const Query = {
-  dogs(parent, args, context, info){
-    return [{name: 'Snickers'}, {name: 'Sunny'}]
+//passes the logic of the query back to the DB directly
+const {forwardTo} = require('prisma-binding');
 
-  }
+const Query = {
+  stories: forwardTo('db'),
+//  async stories(parent,args,ctx,info){
+//    const stories = await ctx.db.query.stories();
+//
+//    return stories;
+//  }
+//  dogs(parent, args, context, info){
+//    global.dogs = global.dogs || [];
+//    return global.dogs;
+//
+//  }
 };
 
 module.exports = Query;
