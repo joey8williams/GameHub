0a552d0c6377e03ef808fa65cf79d173003499f8
Roodey <joey8williams@gmail.com>
2018-11-03 08:16:05 -0400

    apollo is setup. All setup with the exception of authentication is done.

--- full-stack/frontend/pages/_app.js
+++ full-stack/frontend/pages/_app.js
@@ -3,21 +3,35 @@ import Page from '../components/Page/Page';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAngleDoubleLeft} from '@fortawesome/free-solid-svg-icons';
+import {ApolloProvider} from 'react-apollo';
+import withData from '../lib/withData';
 
 library.add(faAngleDoubleLeft);
 
 class MyApp extends App{
+    static async getInitialProps({Component, ctx}){
+        let pageProps = {};
+        if(Component.getInitialProps){
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        pageProps.query = ctx.query;
+        return {pageProps};
+    }
+
     render(){
-        const {Component} = this.props;
+
+        const {Component, apollo, pageProps} = this.props;
 
         return (
             <Container>
-                <Page>
-                    <Component />
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </ApolloProvider>
             </Container>
         )
     }
 
 }
-export default MyApp;
+export default withData(MyApp);
