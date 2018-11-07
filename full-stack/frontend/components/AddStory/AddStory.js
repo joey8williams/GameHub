b6e16c13df1ec37dd2d78f835b1df377ddf28dbb
Roodey <joey8williams@gmail.com>
2018-11-07 11:18:20 -0500

    started working on more front end stuff, but it will require more UI testing than I can do at work, swapping to backend again

---
+++ full-stack/frontend/components/AddStory/AddStory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Mutation} from 'graphql-tools';
+
+export default class AddStory extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ full-stack/frontend/components/styles/Input.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+//Go ahead and start referencing this for inputs so it isn't a huge refactoring effort later 
+export const Input = styled.input``;
+
