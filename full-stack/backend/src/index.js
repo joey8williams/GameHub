a5f7770dffc30d73885c5d6d85f7022305850dab
Roodey <joey8williams@gmail.com>
2018-11-13 07:22:40 -0500

    back end work for finding the currently authenticated user

--- full-stack/backend/src/index.js
+++ full-stack/backend/src/index.js
@@ -1,5 +1,6 @@
 //Starts up node server
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({path:'variables.env'});
 const createServer = require('./createServer');
@@ -8,7 +9,17 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-//TODO: Use middleware to populate current user 
+
+//decode JWT so we can get the userId on each request
+server.express.use((req,res,next) => {
+    const {token} = req.cookies;
+    if(token){
+        const userId = jwt.verify(token, process.env.APP_SECRET);
+        //put the userId onto the request for more requests
+        req.userId = userId;
+    }
+    next();
+});
 
 server.start({
     cors: {
--- full-stack/backend/src/resolvers/Query.js
+++ full-stack/backend/src/resolvers/Query.js
@@ -3,7 +3,18 @@ const {forwardTo} = require('prisma-binding');
 
 const Query = {
   stories: forwardTo('db'),
-  users: forwardTo('db')
+  users: forwardTo('db'),
+  me(parent,args,ctx,info){
+    //check if there is a current userId
+    if(!ctx.request.userId){
+      return null;
+    }
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId }
+    }, info
+    );
+
+  }
 //  async stories(parent,args,ctx,info){
 //    const stories = await ctx.db.query.stories();
 //
